14d9461a0dffe66cf9895f8be404fd895e8782eb
Nathan-Henry7 <158782963+Nathan-Henry7@users.noreply.github.com>
2025-04-30 11:03:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('En forme les gars\n');
+    res.end('En forme \n');
 });
 
 
